c2a019d40f532741f44625d80d3e7d4cc7c2bf58
James Kennedy <jim@hafnartollur.HAF>
2022-04-29 13:49:51 +0000

    Added European plaice

---
+++ European plaice - Landings .R
@@ -0,0 +1,127 @@
+library(icesSAG)
+library(tidyverse)
+
+#Get the assesment keys for the year
+assessmentKeys <- findAssessmentKey("ple", 2020)
+#https://standardgraphs.ices.dk/stockList.aspx
+
+#Download all the data
+# cod <- getStockDownloadData(assessmentKeys)
+# 
+# cod <- data.frame(cod)
+
+#Find out which stock belongs to which key
+head(data.frame(getStockDownloadData(14396))) %>% select("AssessmentKey", "StockDescription", "AssessmentYear")
+
+
+ple_Kattegat <- data.frame(getStockDownloadData(14138))%>% 
+  mutate(ICES_Areas = as.character(ICES_Areas)) %>% 
+  mutate(across(contains('Unit'), ~ as.character(.)))%>% 
+  select("AssessmentKey", "AssessmentYear", "StockKeyLabel","StockDescription", "Year", "Landings", "OfficialLandings", "Catches")
+
+ple_Baltic <- data.frame(getStockDownloadData(14571))%>% 
+  mutate(ICES_Areas = as.character(ICES_Areas)) %>% 
+  mutate(across(contains('Unit'), ~ as.character(.)))%>% 
+  select("AssessmentKey", "AssessmentYear", "StockKeyLabel","StockDescription", "Year", "Landings", "OfficialLandings", "Catches")
+
+ple_North <- data.frame(getStockDownloadData(15614))%>% 
+  mutate(ICES_Areas = as.character(ICES_Areas)) %>% 
+  mutate(across(contains('Unit'), ~ as.character(.)))%>% 
+  select("AssessmentKey", "AssessmentYear", "StockKeyLabel","StockDescription", "Year", "Landings", "OfficialLandings", "Catches")
+
+ple_Irish <- data.frame(getStockDownloadData(14392)) %>% 
+  mutate(ICES_Areas = as.character(ICES_Areas)) %>% 
+  mutate(across(contains('Unit'), ~ as.character(.))) %>% 
+  select("AssessmentKey", "AssessmentYear", "StockKeyLabel","StockDescription", "Year", "Landings", "OfficialLandings", "Catches")
+
+ple_east_channel <- data.frame(getStockDownloadData(14598))%>% 
+  mutate(ICES_Areas = as.character(ICES_Areas)) %>% 
+  mutate(across(contains('Unit'), ~ as.character(.)))%>% 
+  select("AssessmentKey", "AssessmentYear", "StockKeyLabel", "StockDescription","Year", "Landings", "OfficialLandings", "Catches")
+
+ple_west_channel <- data.frame(getStockDownloadData(14426))%>% 
+  mutate(ICES_Areas = as.character(ICES_Areas)) %>% 
+  mutate(across(contains('Unit'), ~ as.character(.)))%>% 
+  select("AssessmentKey", "AssessmentYear", "StockKeyLabel", "StockDescription","Year", "Landings", "OfficialLandings", "Catches")
+
+ple_Bristol <- data.frame(getStockDownloadData(15646))%>% 
+  mutate(ICES_Areas = as.character(ICES_Areas)) %>% 
+  mutate(across(contains('Unit'), ~ as.character(.)))%>% 
+  select("AssessmentKey", "AssessmentYear", "StockKeyLabel","StockDescription", "Year", "Landings", "OfficialLandings", "Catches")
+
+ple_celtic_south <- data.frame(getStockDownloadData(15644))%>% 
+  mutate(ICES_Areas = as.character(ICES_Areas)) %>% 
+  mutate(across(contains('Unit'), ~ as.character(.)))%>% 
+  select("AssessmentKey", "AssessmentYear", "StockKeyLabel", "StockDescription","Year", "Landings", "OfficialLandings", "Catches")
+
+stocknames <- c("Kattegat, Belt Seas, and the Sound",
+                "Baltic Sea",
+                "North Sea and Skagerrak", 
+                "Irish Sea",
+                "eastern English Channel",
+                "western English Channel",
+                "Bristol Channel, Celtic Sea",
+                "Celtic Sea South, southwest of Ireland")
+
+dfs <- lapply(ls(pattern="^ple"), function(x) get(x))
+
+ple <- bind_rows(dfs, .id="stocknum") %>% 
+  mutate (Stock = case_when(StockKeyLabel == "ple.27.24-32" ~ "Baltic Sea", 
+                            StockKeyLabel == "ple.27.7fg" ~ "Bristol Channel, Celtic Sea",
+                            StockKeyLabel == "ple.27.7h-k" ~ "Celtic Sea South, southwest of Ireland",
+                            StockKeyLabel == "ple.27.7d" ~ "eastern English Channel",
+                            StockKeyLabel == "ple.27.7a" ~ "Irish Sea",
+                            StockKeyLabel == "ple.27.21-23" ~ "Kattegat, Belt Seas, and the Sound",
+                            StockKeyLabel == "ple.27.420" ~ "North Sea and Skagerrak",
+                            StockKeyLabel == "ple.27.7e" ~ "western English Channel")) %>% 
+  filter(Year < 2022) %>% 
+  mutate(Landings = case_when (is.na(Landings) ~ Catches, TRUE ~ Landings)) %>% 
+  mutate(Stock = as.factor(Stock)) %>% 
+  mutate(Stock = fct_reorder(Stock, Landings, .fun= sum, na.rm = TRUE))
+
+#data from https://www.hafogvatn.is/is/veidiradgjof/skarkoli
+ple_Iceland <- read.csv("Data/ple_landings_Iceland.csv") %>% 
+  select(Year = year, Landings = landings) %>% 
+  group_by(Year) %>% 
+  summarise(Landings = sum(Landings)) %>% 
+  mutate(Stock = "Iceland")
+
+ple <- bind_rows(ple, ple_Iceland) %>% 
+  mutate(Stock = fct_reorder(Stock, Landings, .fun= sum, na.rm = TRUE))
+
+#data from MSC https://fisheries.msc.org/en/fisheries/gela-ltd-north-east-atlantic-european-plaice/@@view
+ple_Barents <- read.csv("Data/ple_landings_Barents.csv") 
+
+ple <- bind_rows(ple, ple_Iceland, ple_Barents) %>% 
+  mutate(Stock = fct_reorder(Stock, Landings, .fun= sum, na.rm = TRUE))
+
+
+
+
+
+#Total landings for time period
+ple %>% group_by(Stock) %>% summarise(sum = sum(Landings, na.rm = TRUE)) 
+
+ple_g1 <- ple %>% filter(Year < 2022) %>% 
+  
+ggplot(aes(Year, y =Landings/1000, fill = Stock))+
+  geom_bar(stat="identity", position="stack", colour="black", width = 1)+
+  scale_y_continuous (limits = c(0,220), breaks=seq(0, 300, by=20),expand=c(0,0) ) +
+  scale_x_continuous (expand=c(0,0), limits = c(1959.3,2021.2), breaks = seq(0,2020, by=5))+
+  labs(x = "Year", y="Landings (thousand tonnes)", fill = "Stock")+
+  viridis::scale_fill_viridis(discrete = TRUE, direction = 1, option="inferno")+
+  theme(axis.line = element_line(size = 0.7, colour = "black"),
+        axis.ticks = element_line(size = 1),
+        axis.text = element_text (size = 18, colour = "black"),
+        axis.title = element_text (size = 26, colour = "black"),
+        plot.title = element_text(hjust = 0.5),
+        legend.title=element_text(size=24),
+        legend.text=element_text(size=20),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank())
+ple_g1
+
+jpeg("European plaice landings.jpeg", width = 1400, height = 700)
+ple_g1
+dev.off()
